23fc30ef317504d6b426ab3381629b24d5881774
jamespanter <james.panter@blueyonder.co.uk>
2020-06-04 22:36:47 +0100

    js logic to stop audio on button press

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const soundboardContainer = document.getElementById("soundboard-container");
+const audioContainer = document.getElementById("audio-container");
 
 const items = [
   {
@@ -65,14 +66,32 @@ const mapSoundboardItems = () => {
       <img src=../assets/${item.imageFileName} class="slide-in-left delay1">
       <div class="card-text" class="slide-in-left">
       <h2 class="slide-in-left delay2" >${item.title}</h2>
-      <button onclick=playSound("${item.soundFileName}") class="slide-in-left delay2">PLAY</button> 
+      <button onclick=playSound("${item.soundFileName}") class="slide-in-left delay2"><i class="fa fa-play" aria-hidden="true"></i></button> 
       </div></div>`;
     })
     .join("");
 };
 
 const playSound = (soundFileName) => {
-  audio = new Audio(`../assets/${soundFileName}`);
-  audio.pause();
-  audio.play();
+  stopAudio();
+  document.getElementById(soundFileName).play();
 };
+
+const mapAudioElements = () => {
+  audioContainer.innerHTML = items
+    .map((item) => {
+      return `<audio id="${item.soundFileName}" preload="auto"><source src=../assets/${item.soundFileName} type="audio/wav"></audio>`;
+    })
+    .join("");
+};
+
+const stopAudio = () => {
+  for (let i = 0; i < items.length; i++) {
+    const audioFile = document.getElementById(items[i].soundFileName);
+    audioFile.currentTime = 0;
+    audioFile.pause();
+  }
+};
+
+mapSoundboardItems();
+mapAudioElements();
