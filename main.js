c80be440a71341304c91bebf25c1411f58680717
jamespanter <james.panter@blueyonder.co.uk>
2020-06-04 20:42:43 +0100

    Entry animation

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ const soundboardContainer = document.getElementById("soundboard-container");
 
 const items = [
   {
-    title: "BIRD 1",
+    title: "BIRD1",
     imageFileName: "bird1.png",
     soundFileName: "bird1.wav",
   },
   {
-    title: "BIRD 1",
+    title: "BIRD2",
     imageFileName: "bird2.png",
     soundFileName: "bird2.wav",
   },
   {
-    title: "BIRD 1",
+    title: "BIRD3",
     imageFileName: "bird3.png",
     soundFileName: "bird3.wav",
   },
   {
-    title: "BIRD 1",
+    title: "BIRD4",
     imageFileName: "bird4.png",
     soundFileName: "bird4.wav",
   },
@@ -27,10 +27,10 @@ const mapSoundboardItems = () => {
   soundboardContainer.innerHTML = items
     .map((item) => {
       return `<div class="card" id=${item.title}>
-      <img src=../assets/${item.imageFileName}>
-      <div class="card-text">
-      <h2>${item.title}</h2>
-      <button onclick=playSound("${item.soundFileName}")>PLAY</button> 
+      <img src=../assets/${item.imageFileName} class="slide-in-left delay1">
+      <div class="card-text" class="slide-in-left">
+      <h2 class="slide-in-left delay2" >${item.title}</h2>
+      <button onclick=playSound("${item.soundFileName}") class="slide-in-left delay3">PLAY</button> 
       </div></div>`;
     })
     .join("");
